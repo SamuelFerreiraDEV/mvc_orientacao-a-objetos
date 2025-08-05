876a9c19b62fa8a3e9f4f7a4ba75c50173354446
samuelferreira <contato.ferreirasamuel@gmail.com>
2025-08-05 13:33:40 -0300

    feat(dao): added databaseDAO for author

---
+++ src/main/java/projetomvc/models/dao/author/DatabaseAuthorDAO.java
@@ -0,0 +1,108 @@
+package projetomvc.models.dao.author;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import projetomvc.models.dao.interfaces.DAO;
+import projetomvc.models.entities.Author;
+
+public class DatabaseAuthorDAO implements DAO<Author>  {
+    private Connection connection;
+
+    public DatabaseAuthorDAO(Connection connection) {
+      	this.connection = connection;
+    }
+
+    @Override
+	public boolean save(Author author) throws SQLException {
+		String sql = "INSERT INTO author (name, hometown, birth_date) VALUES (?, ?, ?)";
+
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setString(1, author.getName());
+			stmt.setString(2, author.getHometown());
+			stmt.setInt(3, author.getBirthDate());
+
+			int rowsAffected = stmt.executeUpdate();
+			return rowsAffected > 0;
+		} catch (SQLException e) {
+			throw e;
+		}
+    }
+
+	@Override
+	public boolean update(int authorId, Author author) throws SQLException {
+		String sql = "UPDATE author SET name = ?, hometown = ?, birth_date = ? WHERE id = ?";
+
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setString(1, author.getName());
+			stmt.setString(2, author.getHometown());
+			stmt.setInt(3, author.getBirthDate());
+			stmt.setInt(4, authorId);
+
+			int rowsAffected =  stmt.executeUpdate();
+			return rowsAffected > 0;
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+	
+	@Override
+	public boolean delete(int authorId) throws SQLException {
+		String sql = "DELETE FROM author WHERE id = ?";
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setInt(1, authorId);
+	
+			int rowsAffected =  stmt.executeUpdate();
+			return rowsAffected > 0;
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+
+	@Override
+	public Author find(int authorId) throws SQLException {
+		String sql = "SELECT * FROM author WHERE id = ?";
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setInt(1, authorId);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				return new Author(
+					rs.getInt("id"),
+					rs.getString("name"),
+					rs.getString("hometown"),
+					rs.getInt("birth_date")
+				);
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+		return null;
+	}
+
+	@Override
+	public List<Author> findAll() throws SQLException {
+		String sql = "SELECT * FROM author;";
+		
+		try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+			ResultSet rs = stmt.executeQuery();
+
+			List<Author> authors = new ArrayList<>();
+			while (rs.next()) {
+				authors.add( new Author(
+					rs.getInt("id"),
+					rs.getString("name"),
+					rs.getString("hometown"),
+					rs.getInt("birth_date")
+				));
+			}
+			
+			return authors;
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+}
